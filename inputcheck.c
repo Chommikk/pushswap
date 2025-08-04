8a27cf3cdcf9af2543d187ab7d794f9a6b797ba7
Michal Choma <mchoma@c3r1p2.42vienna.com>
2025-08-04 11:49:26 +0200

    norminettet input check and edgcases

--- inputcheck.c
+++ inputcheck.c
@@ -9,7 +9,6 @@
 /*   Updated: 2025/07/29 13:18:20 by mchoma           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
 #include "libft.h"
 #include "pushswap.h"
 
@@ -19,20 +18,20 @@ long	*split_to_arr(char **arr)
 	long	*stack;
 
 	i = 0;
-	while(arr[i])
+	while (arr[i])
 		i ++;
 	stack = malloc (sizeof(long) * (i + 1));
 	if (stack == NULL)
 		return (free_split(arr), NULL);
 	i = 0;
-	while(arr[i]) 
+	while (arr[i])
 	{
 		stack[i] = satoi_safe(arr[i]);
 		if (stack [i] == (long) INT_MAX + 1)
 			return (free_split(arr), free(stack), NULL);
 		i ++;
 	}
-	stack[i] = (long)INT_MIN  -1;
+	stack[i] = (long)INT_MIN -1;
 	free_split(arr);
 	return (stack);
 }
@@ -53,7 +52,8 @@ long	satoi_safe(char *str)
 	while (str[i] && str[i] <= '9' && str[i] >= '0')
 	{
 		number = number * 10 + str[i] - '0';
-		if ((pon == 1 && number > (long)INT_MAX) || (pon == -1 &&  pon * number < (long)INT_MIN))
+		if ((pon == 1 && number > (long)INT_MAX)
+			|| (pon == -1 && pon * number < (long)INT_MIN))
 			return ((long)INT_MAX + 1);
 		i ++;
 	}
@@ -68,23 +68,23 @@ long	*append_long(long *stack, long thing)
 	size_t	i;
 	long	*ptr;
 
-	if(stack == NULL)
+	if (stack == NULL)
 	{
 		ptr = malloc (sizeof(long) * 2);
-		if(!ptr)
+		if (!ptr)
 			return (NULL);
 		ptr[0] = thing;
 		ptr[1] = (long)INT_MIN - 1;
 		return (ptr);
 	}
 	i = 0;
-	while(stack[i] != (long)INT_MIN - 1)
+	while (stack[i] != (long)INT_MIN - 1)
 		i ++;
 	ptr = malloc (sizeof(long) * i + 2);
 	if (ptr == NULL)
 		return (free(stack), ptr);
 	i = 0;
-	while(stack[i ++] != (long)INT_MIN - 1)
+	while (stack[i ++] != (long)INT_MIN - 1)
 		ptr[i - 1] = stack[i - 1];
 	ptr[i - 1] = thing;
 	ptr[i] = (long)INT_MIN - 1;
@@ -98,7 +98,7 @@ void	free_split(char **arr)
 	i = 0;
 	if (arr)
 	{
-		while(arr[i])
+		while (arr[i])
 		{
 			free(arr[i]);
 			i ++;
